808123c5267c86c5e852d37bf270d91037a5de47
Roland Michelberger <>
2023-08-26 14:57:57 +0200

    - fix: marked init() as public

--- Sources/OkHttpClient/Decoding/RawDataDecoder.swift
+++ Sources/OkHttpClient/Decoding/RawDataDecoder.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public final class RawDataDecoder: DataDecoder {
+    public init() {}
+    
     public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable {
         if let data = data as? T {
             return data
