39d386ff81ee6560d650ddad18fc744a8885afce
Roland Michelberger <>
2023-08-26 14:49:27 +0200

    - added RawDataDecoder

---
+++ Sources/OkHttpClient/Decoding/RawDataDecoder.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public final class RawDataDecoder: DataDecoder {
+    public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable {
+        if let data = data as? T {
+            return data
+        } else { throw URLError(.cannotDecodeRawData) }
+    }
+}
